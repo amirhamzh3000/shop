4d1e37873100c4b0265b7d7207f9b01b2dbe451c
amirhamzh <amirhamzh3000@gmail.com>
2024-06-04 19:28:42 +0330

    first commit

---
+++ a/accounts/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255,unique=True)
+    phone_number = models.CharField(max_length=11,unique=True)
+    full_name = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email','full_name']
+
+
+
+    def __str__(self) :
+        return self.email
+    
+
+    def has_perm (self , perm ,obj = None):
+        return True
+    
+
+    def has_module_perm (self , app_label):
+        return True 
+    
+    @property
+    def is_staff(slef):
+        return slef.is_admin
